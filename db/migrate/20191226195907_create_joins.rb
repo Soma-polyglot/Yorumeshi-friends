7eecad595d1bba619f763a68e7a8e73c5d1047af
Soma <senga.s.0801@gmail.com>
2019-12-28 00:04:45 +0900

    不要なDBを削除

--- db/migrate/20191226195907_create_joins.rb
+++
@@ -1,9 +0,0 @@
-class CreateJoins < ActiveRecord::Migration[5.0]
-  def change
-    create_table :joins do |t|
-      t.references :user,   null: false, foreign_key: true
-      t.references :post,   null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191226195907) do
+ActiveRecord::Schema.define(version: 20191119051327) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",                  null: false
@@ -27,15 +27,6 @@ ActiveRecord::Schema.define(version: 20191226195907) do
     t.string   "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "joins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id",    null: false
-    t.integer  "post_id",    null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_joins_on_post_id", using: :btree
-    t.index ["user_id"], name: "index_joins_on_user_id", using: :btree
   end
 
   create_table "post_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -79,8 +70,6 @@ ActiveRecord::Schema.define(version: 20191226195907) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
-  add_foreign_key "joins", "posts"
-  add_foreign_key "joins", "users"
   add_foreign_key "post_images", "posts"
   add_foreign_key "posts", "genres"
   add_foreign_key "posts", "users"
