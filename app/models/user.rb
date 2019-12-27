4a8965828122cd9c62a1a068e175087e1c27867c
Soma <senga.s.0801@gmail.com>
2019-12-28 00:24:08 +0900

    user登録時のvalidates追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :post_users
   mount_uploader :image, ImageUploader
+
+  # validates
+  validates :email,                   presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password,                presence: true, length: {minimum: 6, maximum: 128}
 end
