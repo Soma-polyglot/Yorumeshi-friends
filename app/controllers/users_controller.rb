54d34d9a2af271ec62aa312f65f0b42b9b3996f4
Soma <senga.s.0801@gmail.com>
2020-01-03 17:15:59 +0900

    保存先がALLだったミスを個別へ直しました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    User.update(user_params)
+    @user.update(user_params)
     redirect_to user_path(@user.id)
   end
 
